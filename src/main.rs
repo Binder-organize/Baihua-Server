d025dd9d7f1786b2152d2f2ed6e3b3c912c52b7a
Gavin Zheng <gav.zheng@outlook.com>
2026-08-20 22:17:49 +0800

    style: No more war, Peace is our dream.

--- src/main.rs
+++ src/main.rs
@@ -53,13 +53,16 @@ async fn main() -> Result<()> {
     }
 
     println!(
-        "Baihua Server - v0.1.3 ({})",
+        "Baihua Server - v0.1.3 ({}) by Gavin Zheng et al.",
         if environment.is_production() {
             "production"
         } else {
             "development"
         }
     );
+
+    println!("Baihua is an open-source software distributed under the Apache License, Version 2.0.");
+    println!("No more war, Peace is our dream.");
 
     // Initialize the server.
     let (state, log_guard) = match initialize::initialize(environment).await {
